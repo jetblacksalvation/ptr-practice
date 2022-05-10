4ef192f0cb7bdeadc3851191313936d7cf2a1f3a
jetblacksavior <coolanimeguy666@gmail.com>
2022-05-10 10:50:23 -0600

    added stuff

--- cringearray.cpp
+++ cringearray.cpp
@@ -3,32 +3,31 @@
 
 #include <iostream>
 #include<array>
-template <typename type> class Array {
+template <typename type> class Array {//template typename has to go BEFORE the class
 public:
     void* ptr;
 
-    Array(type pushto) {
-        ptr = &pushto;
+    Array(const type pushto) {//push this variables address to ptr, which can have anytype, just needs to be casted to correct type
+        ptr = new type(pushto);
     };
-    ~Array() { delete ptr; };
+    ~Array() { delete ptr; };//deconstructor, memory leaks go bye bye? 
 
-    type returnval() {
-        return static_cast<type>(ptr);//this don work :(
+    std::string returnval() {
+        return typeid(static_cast<type*>(ptr)).name();
     };
 
-    Array operator [](Array b){
 
-    };
 
 };
 int* brul = new int;
-
+int br = 1;
 int main()
 {
     std::cout << "Hello World!\n";
-    Array<int>  bruh(2);
-
-    std::cout << bruh.ptr << std::endl;
+    Array<int>  bruh(br);
+    //to get this to work you must TYPECAST ptr into the type 
+    std::cout << typeid(bruh.ptr).name() << std::endl;  //changing &bruh.ptr makes it a pointer to a pointer, do not do that 
+    std::cout << bruh.returnval() << std::endl;
     
 
 }
